6fa53b89f1d1165280d94d2f36650d376405510c
trailjeep <trailjeep@gmail.com>
2020-05-24 09:46:14 -0400

    code cleanup -tabs

--- syntax.php
+++ syntax.php
@@ -20,11 +20,11 @@ class syntax_plugin_tcycle extends DokuWiki_Syntax_Plugin {
     function getPType(){ return 'normal';}
     function getAllowedTypes() { return array('container','substition','protected','disabled','formatting','paragraphs'); }
     function getSort(){ return 195; }
-	function connectTo($mode) {
-		$this->Lexer->addEntryPattern('<tcycle.*?>(?=.*?</tcycle>)',$mode,'plugin_tcycle');
-		$this->Lexer->addPattern('\{\{.*\}\}','plugin_tcycle');
-	}
-	function postConnect() { $this->Lexer->addExitPattern('</tcycle>','plugin_tcycle'); }
+    function connectTo($mode) {
+        $this->Lexer->addEntryPattern('<tcycle.*?>(?=.*?</tcycle>)',$mode,'plugin_tcycle');
+        $this->Lexer->addPattern('\{\{.*\}\}','plugin_tcycle');
+    }
+    function postConnect() { $this->Lexer->addExitPattern('</tcycle>','plugin_tcycle'); }
   
     /**
      * Handle the match
@@ -35,29 +35,29 @@ class syntax_plugin_tcycle extends DokuWiki_Syntax_Plugin {
                 $attributes  = strtolower(substr($match, 5, -1));
                 $dataspeed   = $this->_getAttribute($attributes, "speed", $this->getConf('speed'));
                 $datafx      = $this->_getAttribute($attributes, "fx", $this->getConf('fx'));
-				if (!in_array($datafx, array('scroll', 'fade'))) { $datafx = $this->getConf('fx'); }
+                if (!in_array($datafx, array('scroll', 'fade'))) { $datafx = $this->getConf('fx'); }
                 $datatimeout = $this->_getAttribute($attributes, "timeout", $this->getConf('timeout'));
-				$width       = $this->_getAttribute($attributes, "width", $this->getConf('width'));
-				$height      = $this->_getAttribute($attributes, "height", $this->getConf('height'));
+                $width       = $this->_getAttribute($attributes, "width", $this->getConf('width'));
+                $height      = $this->_getAttribute($attributes, "height", $this->getConf('height'));
                 $namespace   = $this->_getAttribute($attributes, "namespace", $this->getConf('namespace')); 
-				if ($namespace === 1) {
-					$namespace = str_replace(':', '/', getNS(cleanID(getID())));
-				} elseif ($namespace !== 0) {
-					$namespace = str_replace(':', '/', $namespace);
-				}
-				$metadata    = $this->_getAttribute($attributes, "metadata", $this->getConf('metadata'));
-				$objectfit   = $this->_getAttribute($attributes, "fit", $this->getConf('fit'));
-				if (!in_array($objectfit, array('fill','contain','cover','scale-down','none'))) { $objectfit = $this->getConf('fit'); }
+                if ($namespace === 1) {
+                    $namespace = str_replace(':', '/', getNS(cleanID(getID())));
+                } elseif ($namespace !== 0) {
+                    $namespace = str_replace(':', '/', $namespace);
+                }
+                $metadata    = $this->_getAttribute($attributes, "metadata", $this->getConf('metadata'));
+                $objectfit   = $this->_getAttribute($attributes, "fit", $this->getConf('fit'));
+                if (!in_array($objectfit, array('fill','contain','cover','scale-down','none'))) { $objectfit = $this->getConf('fit'); }
                 return array($state, array($dataspeed, $datafx, $datatimeout, $width, $height, $namespace, $metadata, $objectfit));
-			case DOKU_LEXER_MATCHED:
-				global $conf;
-				$addimgs = trim($match);
-				$addimgs = preg_replace('/\{\{:?/', $conf['mediadir'].'/', $addimgs);
-				$addimgs = preg_replace('/\?.*\}\}/', '', $addimgs);
-				$addimgs = preg_replace('/\|.*?\}\}/', '', $addimgs);
-				$addimgs = str_replace(':', '/', $addimgs);
-				$addimgs = preg_split('/\s+/', $addimgs);
-				return array($state, array($addimgs));
+            case DOKU_LEXER_MATCHED:
+                global $conf;
+                $addimgs = trim($match);
+                $addimgs = preg_replace('/\{\{:?/', $conf['mediadir'].'/', $addimgs);
+                $addimgs = preg_replace('/\?.*\}\}/', '', $addimgs);
+                $addimgs = preg_replace('/\|.*?\}\}/', '', $addimgs);
+                $addimgs = str_replace(':', '/', $addimgs);
+                $addimgs = preg_split('/\s+/', $addimgs);
+                return array($state, array($addimgs));
             case DOKU_LEXER_UNMATCHED:
                 return array($state, $match);
             case DOKU_LEXER_EXIT:
@@ -73,30 +73,30 @@ class syntax_plugin_tcycle extends DokuWiki_Syntax_Plugin {
         if($mode == 'xhtml'){
             list($state,$match) = $data;
             switch ($state) {
-              case DOKU_LEXER_ENTER :
-                list($this->dataspeed, $this->datafx, $this->datatimeout, $this->width, $this->height, $this->namespace, $this->metadata, $this->objectfit) = $match;
-				$renderer->doc .= '<div class="tcycle" style="width: '.$this->width.';"';
-				$renderer->doc .= 'data-speed="'.$this->dataspeed.'" ';
-				$renderer->doc .= 'data-fx="'.$this->datafx.'" ';
-				$renderer->doc .= 'data-timeout="'.$this->datatimeout.'">';
-                break;
-			  case DOKU_LEXER_MATCHED:
-				list($this->addimgs) = $match;
-				break;
-              case DOKU_LEXER_UNMATCHED :  
-                $renderer->doc .= $renderer->_xmlEntities($match);
-                break;
-              case DOKU_LEXER_EXIT :       
-				$images = $this->_getImages($this->namespace, $this->addimgs);
-				$renderer->doc .= $images;
-                $renderer->doc .= '</div>'; 
-                break;
+                case DOKU_LEXER_ENTER :
+                    list($this->dataspeed, $this->datafx, $this->datatimeout, $this->width, $this->height, $this->namespace, $this->metadata, $this->objectfit) = $match;
+                    $renderer->doc .= '<div class="tcycle" style="width: '.$this->width.';"';
+                    $renderer->doc .= 'data-speed="'.$this->dataspeed.'" ';
+                    $renderer->doc .= 'data-fx="'.$this->datafx.'" ';
+                    $renderer->doc .= 'data-timeout="'.$this->datatimeout.'">';
+                    break;
+                case DOKU_LEXER_MATCHED:
+                    list($this->addimgs) = $match;
+                    break;
+                case DOKU_LEXER_UNMATCHED :  
+                    $renderer->doc .= $renderer->_xmlEntities($match);
+                    break;
+                case DOKU_LEXER_EXIT :       
+                    $images = $this->_getImages($this->namespace, $this->addimgs);
+                    $renderer->doc .= $images;
+                    $renderer->doc .= '</div>'; 
+                    break;
             }
             return true;
         }
         return false;
     }
-	
+    
     function _getAttribute($attributeString, $attribute, $default){
         $retVal = $default;
         $pos = strpos($attributeString, $attribute."=");
@@ -108,45 +108,44 @@ class syntax_plugin_tcycle extends DokuWiki_Syntax_Plugin {
             $value = substr($attributeString,$pos);
             
             //replace '=' and quote signs with null and trim leading spaces
-			$value = ltrim(str_replace(['=', "'", '"'], '', $value));
+            $value = ltrim(str_replace(['=', "'", '"'], '', $value));
             
             //grab the text before the next space
             $pos = strpos($value, " ");
             if ($pos > 0) {
                 $value = substr($value,0,$pos);
             }
-            
-			$retVal = hsc($value);
+            $retVal = hsc($value);
         }
         return $retVal;
     }
-	function _getImages($namespace, $addimgs) {
-		global $conf;
+    function _getImages($namespace, $addimgs) {
+        global $conf;
         $files  = array();
-		$images = '';
-		$target = $conf['target']['media'];
-		$relnf  = '';
-		if ($conf['relnofollow'] == 1) { $relnf = 'nofollow'; }
-		if ($namespace !== 0) { $files  = glob($conf['mediadir'].'/'.$namespace."/*.{jp*g,png,gif}", GLOB_BRACE); }
-		$files = array_merge((array)$files, (array)$addimgs);
-		foreach($files as $file) {
-			if (!is_file($file)) { break; }
-			$detail = str_replace($conf['mediadir'], '/_detail', $file);
-			$media  = str_replace($conf['mediadir'], '/_media', $file);
-			$meta  = new JpegMeta($file);
-			$title = $meta->getField('Simple.Title');
-			if ($title === "") { $title = " "; }
-			$alt   = $meta->getField('Iptc.Caption');
-			if ($alt === "") { $alt = " "; }
-			$images .= '<figure>';
-			if ($this->metadata === 1) { $images .= '<figcaption>'.$title.'</figcaption>'; }
-			$images .= '<a href="'.$detail.'" target="'.$target.'" rel ="'.$relnf.' noopener">';
-			$images .= '<img class="media" src="'.$media.'" title="'.$title.'" alt="'.$alt.'" style="object-fit: '.$this->objectfit.'; width: '.$this->width.'; height: '.$this->height.';" />';
-			$images .= '</a>';
-			if ( $this->metadata === 1 ) { $images .= '<figcaption>'.$alt.'</figcaption>'; }
-			$images .= '</figure>';
-       	}
-		return $images;
-	}
+        $images = '';
+        $target = $conf['target']['media'];
+        $relnf  = '';
+        if ($conf['relnofollow'] == 1) { $relnf = 'nofollow'; }
+        if ($namespace !== 0) { $files  = glob($conf['mediadir'].'/'.$namespace."/*.{jp*g,png,gif}", GLOB_BRACE); }
+        $files = array_merge((array)$files, (array)$addimgs);
+        foreach($files as $file) {
+            if (!is_file($file)) { break; }
+            $detail = str_replace($conf['mediadir'], '/_detail', $file);
+            $media  = str_replace($conf['mediadir'], '/_media', $file);
+            $meta  = new JpegMeta($file);
+            $title = $meta->getField('Simple.Title');
+            if ($title === "") { $title = " "; }
+            $alt   = $meta->getField('Iptc.Caption');
+            if ($alt === "") { $alt = " "; }
+            $images .= '<figure>';
+            if ($this->metadata === 1) { $images .= '<figcaption>'.$title.'</figcaption>'; }
+            $images .= '<a href="'.$detail.'" target="'.$target.'" rel ="'.$relnf.' noopener">';
+            $images .= '<img class="media" src="'.$media.'" title="'.$title.'" alt="'.$alt.'" style="object-fit: '.$this->objectfit.'; width: '.$this->width.'; height: '.$this->height.';" />';
+            $images .= '</a>';
+            if ( $this->metadata === 1 ) { $images .= '<figcaption>'.$alt.'</figcaption>'; }
+            $images .= '</figure>';
+        }
+        return $images;
+    }
 }
 //Setup VIM: ex: et ts=4 enc=utf-8 :
